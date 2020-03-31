894af35e6cba24bcb2b1850db6a6e99a607e1416
Benoit de La Forest <benoit.delaforest@margo.com>
2020-04-01 00:53:49 +0200

    Animate player cards
    But only coming from the picked card for now

--- components/Game/PlayerCards/index.js
+++ components/Game/PlayerCards/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import usePlayingCardRefs from '../../../hooks/usePlayingCardRefs';
-import CardSpot from '../../CardSpot';
+import AnimatedPlayingCard from '../../AnimatedPlayingCard';
 import { Row } from '../../layout';
+import CardSpot from '../../CardSpot';
 import PlayingCard from '../../PlayingCard';
+
 import { CardInfo, CardWrapper } from './_styled';
+import { getCardTransitionProps } from '../helpers';
 
 const PlayerCards = ({
   cardBeingWatched,
@@ -16,7 +19,7 @@ const PlayerCards = ({
   shouldRevealAllCards,
   unfoldedCards
 }) => {
-  const { setPlayerCardRef } = usePlayingCardRefs();
+  const { pickedCardRef, getPlayerCardNode, setPlayerCardRef } = usePlayingCardRefs();
 
   const isCardBeingWatched = cardIndex => {
     return (
@@ -28,32 +31,40 @@ const PlayerCards = ({
 
   return (
     <Row spacing="s1_5">
-      {cards.map((card, cardIndex) => (
-        <div key={cardIndex} ref={setPlayerCardRef(cardPlayerId, cardIndex)}>
-          {card ? (
-            <CardWrapper>
-              {(unfoldedCards[cardPlayerId] && unfoldedCards[cardPlayerId][cardIndex]) ||
-              shouldRevealAllCards ? (
-                <PlayingCard card={card} onClick={() => onCardHide(cardIndex, cardPlayerId)} />
-              ) : (
-                // TEMP: keep cards visibile to ease debugging
-                <div style={{ opacity: 0.15 }}>
-                  {isCardBeingWatched(cardIndex) && (
-                    <CardInfo>{cardBeingWatched.player.name} is watching</CardInfo>
-                  )}
-                  <PlayingCard
-                    card={card}
-                    isSelected={selectedCards[cardPlayerId] === cardIndex}
-                    onClick={() => onCardPick(cardIndex, cardPlayerId)}
-                  />
-                </div>
-              )}
-            </CardWrapper>
-          ) : (
-            <CardSpot label="No Card" />
-          )}
-        </div>
-      ))}
+      {cards.map((card, cardIndex) => {
+        const cardNode = getPlayerCardNode(cardPlayerId, cardIndex);
+        const transitionProps = getCardTransitionProps(cardNode, {
+          // TODO: Handle the case where card comes from another player (and not from the picked card)
+          fromNode: pickedCardRef.current
+        });
+        return (
+          <div key={cardIndex} ref={setPlayerCardRef(cardPlayerId, cardIndex)}>
+            {card ? (
+              <CardWrapper>
+                {(unfoldedCards[cardPlayerId] && unfoldedCards[cardPlayerId][cardIndex]) ||
+                shouldRevealAllCards ? (
+                  <PlayingCard card={card} onClick={() => onCardHide(cardIndex, cardPlayerId)} />
+                ) : (
+                  // TEMP: keep cards visibile to ease debugging
+                  <div style={{ opacity: 0.15 }}>
+                    {isCardBeingWatched(cardIndex) && (
+                      <CardInfo>{cardBeingWatched.player.name} is watching</CardInfo>
+                    )}
+                    <AnimatedPlayingCard
+                      card={card}
+                      isSelected={selectedCards[cardPlayerId] === cardIndex}
+                      onClick={() => onCardPick(cardIndex, cardPlayerId)}
+                      transitionProps={transitionProps}
+                    />
+                  </div>
+                )}
+              </CardWrapper>
+            ) : (
+              <CardSpot label="No Card" />
+            )}
+          </div>
+        );
+      })}
     </Row>
   );
 };
